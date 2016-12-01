d63977e44523234f2b80e8f42193f6a4ab5493d5
pramine <pradeep@mail.com>
2016-12-01 23:58:41 +0530

    added

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@
 # - Git v2.6.4
 
 echo "Create docker machine"
-docker-machine create -d virtualbox --virtualbox-memory "6000" docker-ci-tools
+docker-machine create -d virtualbox --virtualbox-memory "6000" ci-tools
 
 echo "Setup environment"
-eval $(docker-machine env docker-ci-tools)
+eval $(docker-machine env ci-tools)
 
 echo "Checkout Git Repository"
 git clone git@github.com:pramine/ci-tools.git
